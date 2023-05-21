10dc12a4543545ae2ae8bd16ee9ec4947259cc1c
Marko IT <xBravix@gmail.com>
2023-05-21 15:01:56 +0200

    Uzupełnienie klasy Samolot.java; dodanie metod;

--- src/Resources/Samolot.java
+++ src/Resources/Samolot.java
@@ -1,4 +1,22 @@
 package Resources;
 
 public class Samolot {
+    private final String nazwa;
+    private final int zasieg, iloscMiejsc;
+
+    public Samolot(String nazwa, int zasieg, int iloscMiejsc){
+        this.nazwa = nazwa;
+        this.zasieg = zasieg;
+        this.iloscMiejsc = iloscMiejsc;
+    }
+    /* *Zwraca nazwę samolotu*/
+    public String getNazwa(){ return nazwa;}
+
+    /* *Zwraca zasięg samolotu*/
+    public int getZasieg(){ return zasieg;}
+
+    /* *Zwraca ilosc miejsc samolotu*/
+    public int getIloscMiejsc(){ return iloscMiejsc;}
+
 }
+
