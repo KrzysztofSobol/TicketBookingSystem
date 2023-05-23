a50c237e6f2e57e72133b35673b3d8356ab23b70
Krzysztof Sobolewski <125202700+KrzysztofSobol@users.noreply.github.com>
2023-05-23 17:43:24 +0200

    Update Lot.java

--- src/Flights/Lot.java
+++ src/Flights/Lot.java
@@ -1,4 +1,10 @@
 package Flights;
 
+import Resources.Samolot;
+
 public class Lot {
+    private String godzina_odlotu;
+    private String dzien;
+    private Samolot samolot;
+    private Trasa trasa;
 }
