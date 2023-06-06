f12730df3bb4adac497f57b2bdd388a78a21957c
Thejwidson <97634046+Thejwidson@users.noreply.github.com>
2023-06-06 20:10:42 +0200

    sporo czegos

--- src/Clients/Klient.java
+++ src/Clients/Klient.java
@@ -1,10 +1,14 @@
 package Clients;
+import Flights.Lot;
+import Resources.Lotnisko;
 
+import javax.swing.plaf.basic.BasicInternalFrameTitlePane;
 import java.util.ArrayList;
 import java.util.List;
 
 public abstract class   Klient {
     public List<Klient> klienci = new ArrayList<>();
+    public List<Lot> Bilety = new ArrayList<>();
 
     public void dodajKlienta(Klient klient){
         klienci.add(klient);
@@ -13,4 +17,27 @@ public abstract class   Klient {
     public void usunKlienta(Klient klient){
         klienci.remove(klient);
     }
+
+    public void kupBilet(Lot lot){
+        if(lot.getDostepne_bilety()<1){
+            System.out.println("Brak dostepnych meijsc na lot");
+        }
+        else {
+            lot.kupowanieBiletu(lot.getDostepne_bilety());
+            Bilety.add(lot);
+            System.out.println("Kupiono bilet o numerze" + Bilety.indexOf(lot) +" na lot z lotniska " + lot.getLotnisko_p() +" do" + lot.getLotnisko_k() + " dnia" + lot.getDzien() + " o godzinie: " + lot.getGodzina_odlotu());
+        }
+    }
+
+    public void odwolajBilet(Lot lot){
+        lot.odwolywanieBiletu(lot.getDostepne_bilety());
+        System.out.println("Odwoloano rezerwacje biletu o numerze" + Bilety.indexOf(lot));
+        Bilety.remove(lot);
+    }
+
+    public void wyswietlBilety(){
+        System.out.println("Bilety klienta" + imie);
+        for()
+    }
+
 }
--- src/Flights/Lot.java
+++ src/Flights/Lot.java
@@ -5,19 +5,22 @@ import Resources.Samolot;
 
 import java.time.DayOfWeek;
 import java.time.LocalTime;
+import java.util.Random;
 
 public class Lot {
     private LocalTime godzina_odlotu;
     private DayOfWeek dzien;
     private Samolot samolot;
     private Lotnisko lotnisko_p, lotnisko_k;
+    private int dostepne_bilety;
 
     public Lot(LocalTime godzina_odlotu, DayOfWeek dzien, Samolot samolot, Lotnisko lotnisko_p, Lotnisko lotnisko_k){
-        this.godzina_odlotu = godzina_odlotu;  //will be random
+        this.godzina_odlotu = godzina_odlotu;  // will be random
         this.dzien = dzien;                    // pon - niedz
         this.samolot = samolot;                // przydzielony algorytmem
         this.lotnisko_p = lotnisko_p;          // po koleji z pętli
         this.lotnisko_k = lotnisko_k;          // po koleji z pętli
+        this.dostepne_bilety=new Random().nextInt(samolot.getIloscMiejsc()+1); // randomowo od 0 do ilosci miejsc w samolocie
     }
     public Lotnisko getLotnisko_p() {
         return lotnisko_p;
@@ -31,4 +34,18 @@ public class Lot {
     public DayOfWeek getDzien() { return dzien; }
 
     public LocalTime getGodzina_odlotu() { return godzina_odlotu; }
+
+    public int getDostepne_bilety() {
+        return dostepne_bilety;
+    }
+
+    public void kupowanieBiletu(int dostepne_bilety){ //setter
+        this.dostepne_bilety=dostepne_bilety-1;
+    }
+
+    public void odwolywanieBiletu(int dostepne_bilety){ //setter
+        this.dostepne_bilety=dostepne_bilety+1;
+    }
+
 }
+
