cb689eba1897dc7238d85087aa1e70d5ba022afd
Marko IT <57179595+MarekTurkowicz@users.noreply.github.com>
2023-06-08 12:32:56 +0200

    Update Firma.java
    data completion - class Firma

--- src/Clients/Types/Firma.java
+++ src/Clients/Types/Firma.java
@@ -3,4 +3,9 @@ package Clients.Types;
 import Clients.Klient;
 
 public class Firma extends Klient {
+    private final String nazwa, numer_KRS;
+    public Firma(String Nazwa, String Numer_KRS){
+        this.nazwa = Nazwa;
+        this.numer_KRS = Numer_KRS;
+    }
 }
