d21ff6bcb10ab6e99dcbb858223804d9fd437581
Marko IT <57179595+MarekTurkowicz@users.noreply.github.com>
2023-06-08 12:54:55 +0200

    Update Firma.java
    klasa firma - uzupełnienie

--- src/Clients/Types/Firma.java
+++ src/Clients/Types/Firma.java
@@ -4,7 +4,8 @@ import Clients.Klient;
 
 public class Firma extends Klient {
     private final String nazwa, numer_KRS;
-    public Firma(String Nazwa, String Numer_KRS){
+
+    public Firma(String Nazwa, String Numer_KRS) {
         this.nazwa = Nazwa;
         this.numer_KRS = Numer_KRS;
     }
