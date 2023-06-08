cddefbb4e6829e8ed82fd54ecaec9b504c095d3c
Krzysztof Sobolewski <125202700+KrzysztofSobol@users.noreply.github.com>
2023-06-08 14:25:03 +0200

    Metody Dawida - Main

--- src/Main.java
+++ src/Main.java
@@ -164,4 +164,47 @@ public class Main {
             //}
         }
     }
+     public static void rezerwacjaBiletu(Klient klient, Lot lot){
+        Ticket ticket = new Ticket(lot);
+        ticket.kupBilet(lot,klient);
+        System.out.println("Kupiono bilet na lot z lotniska " + lot.getLotnisko_p().getNazwa() +" do " + lot.getLotnisko_k().getNazwa() + " dnia " + lot.getDzien() + " o godzinie: " + lot.getGodzina_odlotu());
+        klient.dodajBilet(ticket);
+        System.out.println();
+    }
+
+    public static void odwolywanieBiletu(Klient klient, Ticket ticket){
+        ticket.usunBilet(klient,ticket);
+    }
+
+    public static void wyswietlanieBiletu(Klient klient){
+
+        List<Ticket>BiletyKlienta = klient.getBilety();
+        int i=0;
+        if(klient instanceof Osoba) {
+            for (Ticket ticket : BiletyKlienta) {
+                //System.out.println("Bilety klienta: ") + klient;
+                System.out.println((i + 1) + ". " + ticket.getLot().getLotnisko_p().getNazwa() + " -> " + ticket.getLot().getLotnisko_k().getNazwa());
+                System.out.println("    Dnia: " + ticket.getLot().getDzien() + " o godzinie: " + ticket.getLot().getGodzina_odlotu());
+                System.out.println();
+            }
+            if (BiletyKlienta.isEmpty()) {
+                System.out.println("Klient nie posiada biletow");
+                System.out.println();
+            }
+        }
+        else{
+            if(klient instanceof Firma){
+                for (Ticket ticket : BiletyKlienta) {
+                    //System.out.println("Bilety klienta: ") + klient;
+                    System.out.println((i + 1) + ". " + ticket.getLot().getLotnisko_p().getNazwa() + " -> " + ticket.getLot().getLotnisko_k().getNazwa());
+                    System.out.println("    Dnia: " + ticket.getLot().getDzien() + " o godzinie: " + ticket.getLot().getGodzina_odlotu());
+                    System.out.println();
+                }
+                if (BiletyKlienta.isEmpty()) {
+                    System.out.println("Klient nie posiada biletow");
+                    System.out.println();
+                }
+            }
+        }
+    }
 }
