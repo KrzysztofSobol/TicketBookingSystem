81e02d09b58ec8f29cdc751430cec58b4c177db9
Krzysztof Sobolewski <125202700+KrzysztofSobol@users.noreply.github.com>
2023-06-09 15:51:34 +0200

    sort samolotów

--- src/Resources/Samolot.java
+++ src/Resources/Samolot.java
@@ -3,30 +3,40 @@ package Resources;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Comparator;
 import java.util.List;
 
 public class Samolot {
     private final String nazwa;
     private final int zasieg, iloscMiejsc, predkosc;
 
-    public Samolot(String nazwa, int zasieg, int iloscMiejsc, int predkosc){
+    public Samolot(String nazwa, int zasieg, int iloscMiejsc, int predkosc) {
         this.nazwa = nazwa;
         this.zasieg = zasieg;
         this.iloscMiejsc = iloscMiejsc;
         this.predkosc = predkosc;
     }
+
     /* *Zwraca nazwę samolotu*/
-    public String getNazwa(){ return nazwa;}
+    public String getNazwa() {
+        return nazwa;
+    }
 
     /* *Zwraca zasięg samolotu*/
-    public int getZasieg(){ return zasieg;}
+    public int getZasieg() {
+        return zasieg;
+    }
 
     /* *Zwraca ilosc miejsc samolotu*/
-    public int getIloscMiejsc(){ return iloscMiejsc;}
+    public int getIloscMiejsc() {
+        return iloscMiejsc;
+    }
 
-    public int getPredkosc() { return predkosc; }
+    public int getPredkosc() {
+        return predkosc;
+    }
 
-    public static void writeToFile(List<Samolot> planesList,String fileName) {
+    public static void writeToFile(List<Samolot> planesList, String fileName) {
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileName, true))) {
 
             for (Samolot plane : planesList) {
